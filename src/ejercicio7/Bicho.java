7b91a3889193282a4dd35391492b2107d773a315
Javier <gualix_vidal@hotmail.com>
2018-03-03 20:15:28 +0100

    Corrección de los nombres de Bicho en el ejercicio 7

--- src/ejercicio7/Bicho.java
+++ src/ejercicio7/Bicho.java
@@ -54,7 +54,7 @@ public class Bicho extends Thread {
         String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
-        while (salt.length() < 5) { // length of the random string.
+        while (salt.length() < 4) { // length of the random string.
             int index = (int) (rnd.nextFloat() * SALTCHARS.length());
             salt.append(SALTCHARS.charAt(index));
         }
