8ba65b48f40eed32e465aba3b756d15df160c1e8
Javier <gualix_vidal@hotmail.com>
2018-03-03 16:54:51 +0100

    Ejercicio 5 funcionando

--- src/ejercicio5/Pintor.java
+++ src/ejercicio5/Pintor.java
@@ -7,30 +7,27 @@ import java.util.Random;
 public class Pintor extends Thread {
 
     JButton b;
-    Color paleta[] = new Color[5];
+    Color colorin;
     public Pintor (JButton b) {
         this.b = b;
-        paleta[0] = Color.BLACK;
-        paleta[1] = Color.RED;
-        paleta[2] = Color.GREEN;
-        paleta[3] = Color.BLUE;
-        paleta[4] = Color.YELLOW;
-
     }
+
     public void run() {
          while (true) {
              int i = (int) (5. * Math.random());
-             b.setBackground(paleta[i]);
+             b.setBackground(randomColor());
          }
     }
 
     public Color randomColor() {
-        Random a = new Random();
-        float r = a.nextFloat();
-        Random b = new Random();
-        float g = b.nextFloat();
-        Random c = new Random();
-        float b = c.nextFloat();
+        Random rand = new Random();
+        float r = rand.nextFloat();
+        float g = rand.nextFloat();
+        float b = rand.nextFloat();
+
+        Color aleatorio = new Color (r,g,b);
+
+        return aleatorio;
 
     }
 }
